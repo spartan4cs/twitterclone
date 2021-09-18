97f91319badd6860b1659bd5265b73b33e4c41a8
spartan4cs <aakashkumbhare@gmail.com>
2021-09-19 00:09:43 +0530

    Added widgets

--- twitter-clone/src/Widgets.js
+++ twitter-clone/src/Widgets.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import "./Widgets.css"
+import { Search } from "@mui/icons-material";
+import React from "react";
+import "./Widgets.css";
+import {TwitterTweetEmbed,TwitterTimelineEmbed, TwitterShareButton} from "react-twitter-embed"
 function Widgets() {
-    return (
-        <div className="widgets">
-            <h2>This is widget section</h2>
-        </div>
-    )
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <Search className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainter">
+          <h2>What's Happening</h2>
+          <TwitterTweetEmbed tweetId={"1439241500152385539"}/>
+          <TwitterTimelineEmbed sourceType="profile" screenName="linkedinselling" options={{height:400}}/>
+          <TwitterShareButton url={"https://facebook.com/cleverprogrammer"} options={{text:"#react is awesome",via:"linkedinselling"}}/>
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
